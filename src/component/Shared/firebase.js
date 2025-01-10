c04befe30ad88234e2adf7a440ddf13e7f861761
Vortexali <149020057+Vortexali@users.noreply.github.com>
2025-01-11 03:22:18 +0500

    firebase.js

--- src/component/Shared/firebase.js
+++ src/component/Shared/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';  
 const firebaseConfig = {
-  //Your SDK
+  //Your SDK uuxv
 };
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
