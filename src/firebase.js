fa3b764ba2da12c5e38ec5b59321a062933140a1
Syed Muhammad Hamza Ali <syedmhamzaa@gmail.com>
2025-01-10 21:34:21 +0500

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,7 @@ import { getAuth } from 'firebase/auth';
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDs0RTAphmuzctmKDH3MwDPfNu5ypxoas8",
-  authDomain: "vhadb-c09b4.firebaseapp.com",
-  projectId: "vhadb-c09b4",
-  storageBucket: "vhadb-c09b4.appspot.com",
-  messagingSenderId: "1089586920718",
-  appId: "1:1089586920718:web:33fd5205c11a3f5eaa2b5f"
+  // ADD Your Respective SDK
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
